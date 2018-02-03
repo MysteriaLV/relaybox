d3025782518c9a7fa400f91d388774c9b77c1eba
Pavels Veretennikovs <PVeretennikovs@riskfocus.com>
2018-02-03 16:14:06 +0200

    upside down flag

--- src/relay_box_modbus.h
+++ src/relay_box_modbus.h
@@ -2,19 +2,18 @@
 #define RELAY_BOX_MODBUS_H
 
 #define USE_HOLDING_REGISTERS_ONLY
+
 #include <Arduino.h>
 #include <Modbus.h>
 #include <Automaton.h>
 
 //////////////// registers of RELAY_BOX ///////////////////
-enum
-{
-  // The first register starts at address 0
-  ACTIONS,      // Always present, used for incoming actions
+enum {
+    // The first register starts at address 0
+    ACTIONS,      // Always present, used for incoming actions
 
-  // Any registered events, denoted by 'triggered_by_register' in rs485_node of Lua script, 1 and up
-  
-  TOTAL_ERRORS     // leave this one, error counter
+    // Any registered events, denoted by 'triggered_by_register' in rs485_node of Lua script, 1 and up
+    TOTAL_ERRORS     // leave this one, error counter
 };
 
 extern void modbus_setup();
@@ -23,13 +22,24 @@ extern void modbus_set(word event, word value);
 
 extern Atm_led ceiling_lights1, ceiling_lights2, exit_door, smoke_machine;
 
-#define OUT1 PIN2
-#define OUT2 PIN3
-#define OUT3 PIN4
-#define OUT4 PIN5
-#define OUT5 PIN_A3
-#define OUT6 PIN_A2
-#define OUT7 PIN_A1
-#define OUT8 PIN_A0
+#ifdef OUTPUTS_ARE_UPSIDE_DOWN
+    #define OUT8 PIN2
+    #define OUT7 PIN3
+    #define OUT6 PIN4
+    #define OUT5 PIN5
+    #define OUT4 PIN_A3
+    #define OUT3 PIN_A2
+    #define OUT2 PIN_A1
+    #define OUT1 PIN_A0
+#else
+    #define OUT1 PIN2
+    #define OUT2 PIN3
+    #define OUT3 PIN4
+    #define OUT4 PIN5
+    #define OUT5 PIN_A3
+    #define OUT6 PIN_A2
+    #define OUT7 PIN_A1
+    #define OUT8 PIN_A0
+#endif
 
 #endif //RELAY_BOX_MODBUS_H
